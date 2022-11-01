e04fca48c8f490da5509053473ef099de4165fe7
Умут Арпидинов <ymytbek0314@gmail.com>
2022-11-01 17:19:10 +0600

    add clicks, fetch, animations

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".nav-menu")
+const magicButton = document.getElementById("magic")
+const magicImage = document.getElementById("secondImage")
+
 
 hamburger.addEventListener("click",()=>{
     hamburger.classList.toggle("active")
@@ -9,3 +11,35 @@ hamburger.addEventListener("click",()=>{
 document.querySelectorAll(".nav-link").forEach(
     n => n.addEventListener("click",()=>{hamburger.classList.remove("active")
     navMenu.classList.remove("active")}))
+
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+ }
+
+
+
+let ph = [ "img/smile.png","img/smile2.png","img/smile3.png","img/seconddd.png"];
+let co = 0;
+
+magicButton.onclick = function(){
+    let im = document.getElementById("secondImage");
+    im.setAttribute("src",`${ph[co]}`);
+    co++;
+    if(co>3){
+        co = 0;
+    }
+}
+
+const cat_result = document.getElementById("cat_result")
+const cat_btn = document.getElementById("cat_btn")
+
+cat_btn.addEventListener('click',getRandomCat)
+   
+function getRandomCat(){
+    fetch('https://aws.random.cat/meow')
+		.then(res => res.json())
+		.then(data => {
+			cat_result.innerHTML = `<img src=${data.file} alt="cat" />`
+		});
+}
